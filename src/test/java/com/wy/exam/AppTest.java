645accad16abf99c831c01e5dab3d83ef6f056d9
leewooyup <zjadlspun1114@naver.com>
2022-07-05 13:08:34 +0900

    add 함수명을 run으로 변경

--- src/test/java/com/wy/exam/AppTest.java
+++ src/test/java/com/wy/exam/AppTest.java
@@ -7,19 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void 더하기() {
-        int rs = Calculator.add("10 + 20");
+        int rs = Calculator.run("10 + 20");
         assertEquals(30, rs);
     }
 
     @Test
     public void 더하기_2() {
-        int rs = Calculator.add("20 + 20");
+        int rs = Calculator.run("20 + 20");
         assertEquals(40, rs);
     }
 
     @Test
     public void 더하기_3() {
-        int rs1 = Calculator.add("10 + 10");
+        int rs1 = Calculator.run("10 + 10");
         assertEquals(20, rs1);
     }
+
+
 }
